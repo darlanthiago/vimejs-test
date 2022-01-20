98097c91563ea284aeb16d686dcc1bebbd7abb60
Darlan Thiago <darlanthiago53@gmail.com>
2022-01-20 18:13:17 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { DefaultUi, Hls, Player } from "@vime/react";
+
+import video from "./assets/videos/1080p.mp4";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="player-container">
+        <Player autoplay>
+          {/* <Video>
+            <source data-src={video} type="video/mp4" />
+          </Video> */}
+          <Hls poster="https://media.vimejs.com/poster.png">
+            <source
+              data-src="https://media.vimejs.com/hls/index.m3u8"
+              type="application/x-mpegURL"
+            />
+          </Hls>
+          <DefaultUi />
+        </Player>
+      </div>
     </div>
   );
 }
